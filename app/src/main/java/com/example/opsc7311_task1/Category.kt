cacc0697cd92494084e7bb67ef845c43d949949c
DevanConradie <devancon@icloud.com>
2023-09-29 12:03:40 +0200

    Revert "Merge branch 'master' of https://github.com/DevanConradie/OPSC7311-Task1Repo"
    This reverts commit 8661123a95e635f2eb6e312095d9559b23cfe4bc, reversing
    changes made to 764d59b726bce641032e1b51607ac7266787b17f.

--- app/src/main/java/com/example/opsc7311_task1/Category.kt
+++
@@ -1,3 +0,0 @@
-package com.example.opsc7311_task1
-
-data class Category(val id: Long, val name: String)
--- app/src/main/java/com/example/opsc7311_task1/MainActivity.kt
+++ app/src/main/java/com/example/opsc7311_task1/MainActivity.kt
@@ -1,26 +1,11 @@
 package com.example.opsc7311_task1
 
-import android.content.Intent
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.opsc7311_task1.databinding.ActivityMainBinding
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Initialize the View Binding
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-
-        // Set up click listener for the button
-        binding.loginButton.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }
+        setContentView(R.layout.activity_main)
     }
 }
