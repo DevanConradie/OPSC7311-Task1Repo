1f74a874cb23931220450cb849e44493682c06f6
DevanConradie <devancon@icloud.com>
2023-12-04 09:44:41 +0200

    Random Push

---
+++ app/src/main/java/com/example/opsc7311_task1/Statistics.kt
@@ -0,0 +1,11 @@
+package com.example.opsc7311_task1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Statistics : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_statistics)
+    }
+}
